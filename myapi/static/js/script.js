4a9add46f7ca9614443d0fe1e2b58b60b625b828
davidkiama <davidkiama98@gmail.com>
2022-04-05 22:05:06 +0300

    Implemented the GET and POST req with Fetch API

--- myapi/static/js/script.js
+++ myapi/static/js/script.js
@@ -1,44 +1,84 @@
 const list = document.querySelector(".list");
 
-$(document).ready(function () {
-  $(".get-data").submit(function (event) {
-    event.preventDefault();
-    form = $(".get-data");
+// $(document).ready(function () {
+//   $(".get-data").submit(function (event) {
+//     event.preventDefault();
+//     form = $(".get-data");
 
-    $.ajax({
-      url: "/",
-      type: "GET",
-      data: form.serialize(),
-      dataType: "json",
-      success: function (data) {
-        //Data from REST API
-        console.log(data);
-        // alert(data);
+//     $.ajax({
+//       url: "/",
+//       type: "GET",
+//       data: form.serialize(),
+//       dataType: "json",
+//       success: function (data) {
+//         //Data from REST API
+//         console.log(data);
+//         // alert(data);
 
-        //Loop through the data
-        data.forEach((item) => {
-          let element = `<p> ${item.name} </>`;
-          list.insertAdjacentHTML("beforeend", element);
-        });
-      },
-    }); // END of Ajax method
-  }); // End of submit event
+//         //Loop through the data
+//         data.forEach((item) => {
+//           let element = `<p> ${item.name} </>`;
+//           list.insertAdjacentHTML("beforeend", element);
+//         });
+//       },
+//     }); // END of Ajax method
+//   }); // End of submit event
+// });
+
+// $(document).ready(function () {
+//   $(".post-data").submit(function (event) {
+//     event.preventDefault();
+//     form = $(".post-data");
+
+//     $.ajax({
+//       url: "/",
+//       type: "POST",
+//       data: form.serialize(),
+//       dataType: "json",
+//       success: function (data) {
+//         //Data from REST API
+//         console.log(data);
+//       },
+//     }); // END of Ajax method
+//   }); // End of submit event
+// });
+
+const formGetData = document.querySelector(".get-data");
+const formPostData = document.querySelector(".post-data");
+
+formGetData.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  fetch("/")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((item) => {
+        let element = `<p> ${item.name} </>`;
+        list.insertAdjacentHTML("beforeend", element);
+      });
+    });
 });
 
-$(document).ready(function () {
-  $(".post-data").submit(function (event) {
-    event.preventDefault();
-    form = $(".post-data");
+formPostData.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    $.ajax({
-      url: "/",
-      type: "POST",
-      data: form.serialize(),
-      dataType: "json",
-      success: function (data) {
-        //Data from REST API
-        console.log(data);
-      },
-    }); // END of Ajax method
-  }); // End of submit event
+  fetch("/", {
+    // Adding method type
+    method: "POST",
+
+    // Adding body or contents to send
+    body: JSON.stringify({
+      name: formPostData.name.value,
+      alias: formPostData.alias.value,
+    }),
+
+    // Adding headers to the request
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }) // Converting to JSON
+    .then((response) => response.json())
+
+    // Displaying results to console
+    .then((data) => console.log(data));
 });
